8a3307956062012366e4075db1ac0d0d2e37134a
MOHAMED EL FATINE <143297439+ssimofatine@users.noreply.github.com>
2025-03-17 00:40:54 +0100

    Update Casilla.java

--- Casilla.java
+++ Casilla.java
@@ -1,1 +1,22 @@
+package tema8.monopoly.modelo;
 
+public class Casilla {
+    protected String nombre;
+
+    public Casilla(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+      this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + nombre + "]";
+    }
+}
